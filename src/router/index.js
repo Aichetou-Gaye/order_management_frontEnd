4208499434127dc1ab37268fc2cde6b00e8465f2
aichetou1408 <aichetouahmedgaye@gmail.com>
2024-09-20 18:42:17 +0100

    Corrected routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createMemoryHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "../components/Home.vue";
 import Customers from "../components/Customers.vue";
@@ -49,7 +49,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createMemoryHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
 
