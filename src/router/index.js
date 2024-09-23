4184737db299843540ce19c6efe2253889489f07
aichetou1408 <aichetouahmedgaye@gmail.com>
2024-09-24 00:12:03 +0100

    Updated children's route

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import Home from "../components/Home.vue";
-import Customers from "../components/Customers.vue";
+import Home from "@/components/Home.vue";
+import Customers from "@/components/Customers.vue";
 
-import Products from "../components/Products.vue";
+import Products from "@/components/Products.vue";
 
-import Orders from "../components/orders/Orders.vue";
-import AddOrder from "../components/orders/AddOrder.vue";
-import EditOrder from "../components/orders/EditOrder.vue";
-import ViewOrder from "../components/orders/ViewOrder.vue";
+import Orders from "@/components/orders/Orders.vue";
+import AddOrder from "@/components/orders/AddOrder.vue";
+import EditOrder from "@/components/orders/EditOrder.vue";
+import ViewOrder from "@/components/orders/ViewOrder.vue";
 
 const routes = [
   {
@@ -25,19 +25,24 @@ const routes = [
   },
   {
     path: "/order",
-    component: Orders,
-  },
-  {
-    path: "/order/new",
-    component: AddOrder,
-  },
-  {
-    path: "/order/edit/:id",
-    component: EditOrder,
-  },
-  {
-    path: "/order/show/:id",
-    component: ViewOrder,
+    children: [
+      {
+        path: "",
+        component: Orders,
+      },
+      {
+        path: "new",
+        component: AddOrder,
+      },
+      {
+        path: "edit/:id",
+        component: EditOrder,
+      },
+      {
+        path: "show/:id",
+        component: ViewOrder,
+      },
+    ],
   },
 ];
 
